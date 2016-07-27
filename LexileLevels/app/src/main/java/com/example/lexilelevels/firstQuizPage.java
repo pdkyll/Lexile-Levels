272b2916618580463a46d71c414bec8598a5a214
Lily Bhattacharjee <lilybhattacharjeeca@gmail.com>
2016-07-26 17:17:05 -0700

    Added color functionality
    Among the 11 questions coded so far (10 with placeholder text), if the
    incorrect answer is chosen, the selection is turned red and if the
    correct one is chosen, it turns blue.

--- LexileLevels/app/src/main/java/com/example/lexilelevels/firstQuizPage.java
+++ LexileLevels/app/src/main/java/com/example/lexilelevels/firstQuizPage.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -16,45 +17,65 @@ public class firstQuizPage extends AppCompatActivity implements AdapterView.OnIt
 
     TextView question;
     ListView listview;
-    ArrayList<String> list = new ArrayList<String>();
-    String[][] values = { {"A piece of clothing worn on the head", "A stringy root", "A tree", "A feral cat found in South America"} };
+    ArrayAdapter adapter;
+    Button submitAnswer;
+    ArrayList<String> lst = new ArrayList<String>();
+    String[] questions = {"What is the definition of hat?","Question 2","Question 3","Question 4","Question 5","Question 6","Question 7","Question 8",
+            "Question 9","Question 10","Question 11"};
+    String[][] values = { {"A piece of clothing worn on the head", "A stringy root", "A tree", "A feral cat found in South America"},
+            {"q2a1" , "q2a2" , "q2a3" , "q2a4"},{"q3a1" , "q3a2" , "q3a3" , "q3a4"},{"q4a1" , "q4a2" , "q4a3" , "q4a4"},{"q5a1" , "q5a2" , "q5a3" , "q5a4"},
+            {"q6a1" , "q6a2" , "q6a3" , "q6a4"},{"q7a1" , "q7a2" , "q7a3" , "q7a4"},{"q8a1" , "q8a2" , "q8a3" , "q8a4"},{"q9a1" , "q9a2" , "q9a3" , "q9a4"},
+            {"q10a1" , "q10a2" , "q10a3" , "q10a4"},{"q10a1" , "q10a2" , "q10a3" , "q10a4"}};
+    int[] correctAnswerIndices = {0,1,2,3,0,1,2,3,0,1,2};
+    int totalNumberOfQuestions = values.length - 1;
+    int questionNumber = -1;
+    boolean answered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_quiz_page);
-        listview = (ListView) findViewById(R.id.list1);
-
-        for (int i = 0; i < values[0].length; ++i) {
-            list.add(values[0][i]);
-        }
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        listview = (ListView) findViewById(R.id.choices);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, lst);
         listview.setAdapter(adapter);
         listview.setOnItemClickListener(this);
         question = (TextView) findViewById(R.id.firstQuestion);
-        /*answers1 = (ListView) findViewById(R.id.list1);
-        question = (TextView) findViewById(R.id.firstQuestion);
-        ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < choices1.length; ++i) {
-            list.add(choices1[i]);
-           // arrayAdapter1.addItem(choices1[i]);
-        }
-
-
-        arrayAdapter1 = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_2, list);
-        answers1.setAdapter(arrayAdapter1); */
+        question.setText("This is a quiz to determine your English proficiency so that we can recommend level-appropriate books to you. Please" +
+                " press the 'Begin Quiz' button in order to answer the questions.");
+        submitAnswer = (Button) findViewById(R.id.submit);
+        submitAnswer.setText(R.string.beginQuiz);
+        answered = false;
     }
 
     @Override
     public void onItemClick(AdapterView parent, View view, int position, long l) {
-        Log.d("android",position + " : " + list.get(position));
-        view.setBackgroundColor(Color.parseColor(getString(R.color.selectedChoice)));
+        if (!answered) {
+            String colorHighlighted;
+            if (position == correctAnswerIndices[questionNumber]) {
+                colorHighlighted = "#" + Integer.toHexString(getColor(R.color.correctChoice));
+            } else {
+                colorHighlighted = "#" + Integer.toHexString(getColor(R.color.incorrectChoice));
+            }
+            view.setBackgroundColor(Color.parseColor(colorHighlighted));
+            answered = true;
+        }
     }
 
     @Override
     public void onClick(View view) {
+        answered = false;
         question.setText("");
-        list.clear();
+        lst.clear();
+        submitAnswer.setText(R.string.nextQuestion);
+        listview.setAdapter(adapter);
+        if (questionNumber < totalNumberOfQuestions) {
+            questionNumber += 1;
+        } else {
+            questionNumber = totalNumberOfQuestions;
+        }
+        question.setText(questions[questionNumber]);
+        for (int i = 0; i < values[questionNumber].length; ++i) {
+            lst.add(values[questionNumber][i]);
+        }
     }
-
 }
