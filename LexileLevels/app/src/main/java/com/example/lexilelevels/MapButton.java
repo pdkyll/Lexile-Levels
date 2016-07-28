3f5bf926de60cfa3c8ccf746aeae17bbb3f8eb24
margaret84p <margaretp@eastside.org>
2016-07-28 14:54:28 -0700

    Map working
    The map finally showed up now

--- LexileLevels/app/src/main/java/com/example/lexilelevels/MapButton.java
+++ LexileLevels/app/src/main/java/com/example/lexilelevels/MapButton.java
@@ -2,6 +2,7 @@ package com.example.lexilelevels;
 
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,10 +17,6 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapButton extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
 
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +30,24 @@ public class MapButton extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
+/*public class MainActivity extends AppCompatActivity
+        implements ActivityCompat.OnRequestPermissionsResultCallback {
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == MY_LOCATION_REQUEST_CODE) {
+            if (permissions.length == 1 &&
+                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mMap.setMyLocationEnabled(true);
+            } else {
+                // Permission was denied. Display an error message.
+            }
+        }
+    }
+}
+*/
